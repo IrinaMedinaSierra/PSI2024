8a769f2a5a60e660972f4c28260790224d87a601
IrinaMedinaSierra <irina@ceatformacion.com>
2024-01-09 20:44:41 +0100

    primera version

---
+++ unidad1/Actividad1_2.java
@@ -0,0 +1,15 @@
+package unidad1;
+
+import java.util.Scanner;
+
+public class Actividad1_2 {
+
+    public static void main(String[] args) {
+        int num;
+        System.out.print("Indica un numero ");
+        Scanner sn=new Scanner(System.in);
+        num=sn.nextInt();
+        System.out.println("el Numero que escribiste es " + num);
+    }
+
+}
